273d22f7937da3e46c1f5276768665f010e65a34
Rodrigo Garcia <rodrigo.mellogarcia@bridge.ufsc.br>
2020-09-20 22:05:59 -0300

    tarefa 2 adicionada

---
+++ tarefa2/index.js
@@ -0,0 +1,107 @@
+var postContent = [
+    {
+        title:"Time Walk",
+        price:"6500",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/e/0/e0139f60-d48e-46fb-9f5a-1e3d7558c834.jpg?1559591444",
+        description:"No passado, a lista de banimentos de Magic: The Gathering original incluía alguns cards desequilibrados que tinham o potencial de quebrar as regras. O conjunto Power Nine foi obviamente direcionado, mas estamos aqui para falar sobre o card mais famoso de todos. O Time Walk permite que você obtenha um turno extra para dois manas logo após completar o turno atual. Basicamente, você pode fazer vários turnos até derrotar seu inimigo, desde que tenha mana de sobra. Hoje em dia, seria difícil encontrar um modo que ainda permitisse este cartão em jogo, mas como você está pagando muito por ele, você pode tentar. É um cartão MTG muito raro e caro, mas que certamente manteria seu valor ao longo do tempo."
+    }, 
+    {
+        title:"Underground Sea",
+        price:"6650",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/4/3/434ff57b-0bfd-4c17-ad56-909e72f3e2ee.jpg?1559591789",
+        description:"O Mar Subterrâneo é uma carta frequentemente incluída no chamado baralho Bruise. Este deck depende de uma combinação de cartas azuis e pretas, e o Mar Subterrâneo se encaixa perfeitamente na tática, já que é capaz de produzir mana preto ou azul. No entanto, esta carta também tem uma vulnerabilidade séria, principalmente porque conta como ilha e pântano. Portanto, se uma mágica tem como alvo qualquer um desses tipos de terreno e o destrói, a carta também é destruída. Como parte do conjunto original 'Dual Lands', o Mar Subterrâneo é um colecionador muito procurado, especialmente porque foi incluído na Lista Reservada do Magic."
+    },
+    {
+        title:"Mox Emmerald",
+        price:"8500",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/b/0/b0e1427c-05cd-465b-be59-97ed6e39f7ba.jpg?1559591415",
+        description:"Nossa próxima potência Mox é a Mox Emerald, que dá a você a habilidade de adicionar mana verde à sua reserva. Aparentemente, o mana verde é um pouco mais precioso do que outros tipos de mana, já que o Emerald custaria alguns milhares de dólares dependendo de sua raridade e qualidade. Ainda assim, você ainda pode conseguir um desses cartões por muito menos, especialmente se estiver no lugar certo na hora certa. Se você me perguntar, Mox Emerald é o cartão Mox mais bonito que existe, pois apresenta um fundo verde correspondente ao lado da pedra preciosa incrustada em ouro."
+    },
+    {
+        title:"Mox Pearl",
+        price:"8900",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/8/e/8ebe4be7-e12a-4596-a899-fbd5b152e879.jpg?1559591290",
+        description: "Mox Pearl fez parte da nossa lista dos cartões MTG mais caros, pois é um dos cinco Moxen originais. Este cartão apareceu em vários leilões ao longo dos anos, e chegou a ter um preço inicial de $ 250.000 no eBay em algum momento. O que encontramos não custou tanto, mas ainda é uma quantia considerável de dinheiro, mesmo para um cartão MTG. Mox Pearl permite que você adicione White Mana à sua reserva de mana e apresenta uma bela ilustração de pérola assinada por Dan Frazier. Aqueles de vocês que têm a sorte de colocar as mãos em pelo menos três cartas Mox originais vão se divertir muito nos torneios MTG. Ou talvez você apenas ganhe todas as vezes e não seja tão divertido quanto você pensa. Só há uma maneira de descobrir!"
+    },
+    {
+        title:"Timetwister",
+        price:"8999",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/9/a/9a49dc44-616e-4bdd-8220-0bb71eccc512.jpg?1559591443",
+        description:"Timetwister MTG é uma das cartas Power Nine mais interessantes, pois não se encaixa totalmente com o resto do conjunto, pelo menos não de uma forma convencional. A carta estava na fase alfa / beta original do jogo e realmente ajudou a quebrar o jogo na época. No que diz respeito à funcionalidade, Timetwister pode ser colocado de lado em uma nova pilha do cemitério, e o jogador então embaralha sua mão, grimório e cemitério antes de sacar uma nova mão de sete cartas. Todas as cartas são deixadas em jogo onde estão e o oponente deve fazer o mesmo."
+    },
+    {
+        title:"Mox Jet",
+        price:"9000",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/9/2/92bcd1ce-19b1-4d78-8b09-95242ca08d76.jpg?1559591520",
+        description:"As cartas Mox eram bastante caras mesmo quando foram lançadas, e muitos jogadores ficaram preocupados que isso transformasse Magic: The Gathering em um jogo de pagar para ganhar. Nos primeiros turnos, as cinco joias Mox são capazes de gerar muito mana, dando ao jogador uma vantagem considerável sobre seu oponente. Este tipo de jogo de 'rampa' também está presente em outros jogos de cartas modernos, incluindo Hearthstone da Blizzard, particularmente os decks Ramp Druid. Mox Jet adiciona um mana preto à sua reserva de mana a cada turno, o que resulta em muito mana ao longo do jogo. Embora vários cartões Mox tenham recebido ajustes e balanços ao longo dos anos, muitos foram banidos de qualquer forma em vários formatos, e podemos definitivamente entender o porquê."
+    },
+    {
+        title:"Ancestra Recall",
+        price:"14300",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/7/0/70e7ddf2-5604-41e7-bb9d-ddd03d3e9d0b.jpg?1559591549",
+        description:"Como um dos cinco benefícios impressos na série original de Magic: The Gathering, Ancestral Recall é um card muito especial. Embora seu design original fosse um cartão do tipo comum, Richard Garfield decidiu torná-lo raro. No entanto, isso não fez muito para tirar o cartão da potência que é. Pense nisso: basicamente permite que você tire três cartas sem penalidades. Isso pode permitir que você construa uma mão poderosa em nenhum momento. Por esse motivo, Ancestral Recall foi banido de quase todos os formatos, exceto Vintage."
+    },
+    {
+        title:"Mox Sapphire",
+        price:"14999",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/8/2/82da0972-b17b-4600-9efd-e9430a0db04b.jpg?1559591414",
+        description:"A Safira permite que você adicione um Mana Azul à sua reserva de mana a cada turno e, quando combinada com outras joias Mox, pode permitir que você se torne invencível. Você pode imaginar quantos torneios foram ganhos por causa dessas cartas desequilibradas?"
+    },
+    {
+        title:"Volcanic Island",
+        price:"19888",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/1/b/1b386379-a4fd-43e0-9934-8fcd39d36d6a.jpg?1559591575",
+        description:"A Ilha Vulcânica é capaz de produzir mana vermelha ou azul. No entanto, esta carta também tem uma vulnerabilidade séria, principalmente porque conta como ilha e montanha. Portanto, se uma mágica tem como alvo qualquer um desses tipos de terreno e o destrói, a carta também é destruída. Como parte do conjunto original 'Dual Lands', o a Ilha Vulcânica é um item muito procurado, especialmente porque foi incluído na Lista Reservada do Magic."
+    },
+    {
+        title:"Black Lotus",
+        price:"42000",
+        img:"https://c1.scryfall.com/file/scryfall-cards/art_crop/front/b/0/b0faa7f2-b547-42c4-a810-839da50dadfe.jpg?1559591477",
+        description: "Por último, mas não menos importante, o card Magic The Gathering mais caro que existe é o Black Lotus. Este cartão nem estava disponível para venda no momento em que este artigo foi escrito, mas você provavelmente encontrará um leilão que o inclua se procurar bem. Black Lotus é sem dúvida a placa MTG mais famosa de todas. O artefato dá ao jogador três manas da cor que escolher, o que significa que permite que ele retire cartas incrivelmente poderosas no início do jogo. Banido de todos os formatos, exceto Vintage, Black Lotus raramente vê a luz do dia mais. No entanto, colecionadores obstinados ainda vasculham o online e offline para isso regularmente. Normalmente, quando um Black Lotus aparece, ele se esgota em um piscar de olhos, o que também contribui para seu preço relativamente ridículo."
+    }
+]
+
+class Post {
+    constructor(rank, title, price, description, imgUrl) {
+        this.rank = rank;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+
+    toElement() {
+        var title = document.createElement("h1");
+        title.innerHTML = this.rank.toString() + ": " + this.title;
+
+        var price = document.createElement("h2");
+        price.innerHTML = "Preço: US$" + this.price;
+
+        var img = document.createElement("img");
+        img.setAttribute("src", this.imgUrl);
+
+        var description = document.createElement("p");
+        description.innerHTML = this.description;
+
+        var postDiv = document.createElement("div");
+        postDiv.setAttribute("class", "post");
+
+        postDiv.appendChild(title);
+        postDiv.appendChild(price);
+        postDiv.appendChild(description);
+        postDiv.appendChild(img);
+
+        return postDiv;
+    }
+}
+
+function listPosts() {
+    const postList = document.getElementById("posts");
+    postList.innerHTML = "";
+    for (let i = 0; i < postContent.length; i++) {
+        const newPost = new Post(10-i, postContent[i].title, postContent[i].price, postContent[i].description, postContent[i].img);
+        postList.appendChild(newPost.toElement());
+    }
+}
+
+listPosts();
